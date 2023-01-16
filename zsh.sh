c6fb80a0905c0c35e8b08be6a7d61d2ef50738db
vlaine5 <vincent.laine5@gmail.com>
2023-01-16 17:42:51 +0100

    plugin

--- zsh.sh
+++ zsh.sh
@@ -8,13 +8,14 @@ sh -c install.sh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/zsh-syntax-highlighting
-echo "source $HOME/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' $HOME/.zshrc
-sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/' $HOME/.zshrc
-rm -rf $HOME/.p10k.zsh
-rm -rf $HOME/.oh-my-zsh 
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' $HOME/.zshrc
+
+
+#rm -rf $HOME/.p10k.zsh
+#rm -rf $HOME/.oh-my-zsh 
 cp -r .p10k.zsh $HOME
 cp -r .oh-my-zsh $HOME
